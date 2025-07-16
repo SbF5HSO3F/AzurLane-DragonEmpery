9ed1faaba08328b7c148a93ff830dbc1fa52facd
SbF5HSO3F <190558883+SbF5HSO3F@users.noreply.github.com>
2025-07-16 11:40:07 +0800

    东煌古国：添加国家奇观适配

---
+++ ModSupport/GoldenAge/Data/Mode_NationalWonder.sql
@@ -0,0 +1,13 @@
+-- Mode_NationalWonder
+-- Author: HSbF6HSO3F
+-- DateCreated: 2025/7/16 11:32:33
+--------------------------------------------------------------
+INSERT INTO District_Adjacencies
+    (DistrictType,      			    YieldChangeId)
+VALUES
+    --Dragon Garden
+	('DISTRICT_DRAGON_EMPERY_GARDEN',   'GOLDEN_NATIONAL_CAMPUS_SCIENCE'),
+    ('DISTRICT_DRAGON_EMPERY_GARDEN',   'GOLDEN_NATIONAL_CAMPUS_SCIENCE_PLUS'),
+	--Ansteel
+	('DISTRICT_ANSTEEL',                'GOLDEN_NATIONAL_INDUSTRIAL_ZONE_PRODUCTION'),
+    ('DISTRICT_ANSTEEL',                'GOLDEN_NATIONAL_INDUSTRIAL_ZONE_PRODUCTION_PLUS');
